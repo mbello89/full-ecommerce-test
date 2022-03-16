401b28069f9ba47024c36eec0f87c2cc038c9bb0
mbello89 <53487672+mbello89@users.noreply.github.com>
2022-03-16 00:26:36 -0300

    Testing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+// Styled components
+const Container = styled.div `
+    height: 60px;
+`
+const Wrapper = styled.div `
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+const Language = styled.span `
+    font-size: 16px;
+    cursor: pointer;
+`
+const Left = styled.div `
+    flex: 1;
+`
+const Center = styled.div `
+    flex: 1;
+`
+const Right = styled.div `
+    flex: 1;
+`
+
+
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+            </Left>
+            <Center>Test</Center>
+            <Right>Test</Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
